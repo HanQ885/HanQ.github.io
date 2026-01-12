16c37d20bcc863490ad5c660a8f78b4257ef172e
HanQ885 <gs25002@gs.hs.kr>
2026-01-12 22:34:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-a=np.array([1,2,3])
-b=np.array([4,5,6])
+X = np.array([[1, 1],
+              [1, 2],
+              [1, 3]])
 
-print(a+b)
+w = np.array([1, 2])
+
+print(X @ w)
